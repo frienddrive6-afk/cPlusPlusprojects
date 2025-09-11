d0c0147cdff6767fa4f0b7115e32a1c582df2801
frienddrive6-afk <frienddrive6@gmail.com>
2025-09-11 19:34:42 +0300

    update file exercise 2 in lesson 15 classwork

--- shag/lesson15/classwork/exercise_2.cpp
+++ shag/lesson15/classwork/exercise_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>//работа с .txt файлами
 // #include<string>
 // #include <cstring> 
 // #include<cstdarg> //для безконечных аргументов функции
@@ -9,20 +10,106 @@
 // #include <iomanip>
 
 
-
-
 using namespace std;
-
-
-
-
 
 
 
 int main() {
     srand(time(NULL));
 
+    cout<<"Файлы"<<endl;
+
+
+    cout<<"Что именно надо сделать с файлам:\n1)почитать\n2)Записать\n3)Дозаписать\nВаш выбор: ";
+    int choise;
+    cin>>choise;
+    cin.ignore();
+
+
+    string line = "";
+
+
+    if(choise == 2)
+    {
+        //запись файла
+        ofstream fileW("/home/nomantb/c++ trening/shag/lesson15/classwork/txt_felese/file.txt");
+        if(!fileW.is_open())
+        {
+            cout<<"Система не нашла файл"<<endl;
+            return 1;
+        }
+
+        cout<<"Введите * для окончания: ";
     
+        
+        do
+        {
+            getline(cin,line);
+            if(line == "*")
+            {
+                break;
+            }
+
+            fileW<<line<<endl;
+        } while (line != "*");
+
+        fileW.close();
+        
+    }else if(choise == 1)
+    {
+        //чтение файла
+
+        ifstream fileR("/home/nomantb/c++ trening/shag/lesson15/classwork/txt_felese/file.txt");
+
+        if(!fileR)
+        {
+            cout<<"Система не нашла файл"<<endl;
+            return 1;
+        }
+
+        cout<<"Выводим информацию с файла"<<endl;
+
+        while (getline(fileR,line))
+        {
+            cout<<line<<endl;
+        }
+        
+
+
+        fileR.close();
+    }else if(choise == 3)
+    {
+        //Дозапись файла
+        ofstream fileA("/home/nomantb/c++ trening/shag/lesson15/classwork/txt_felese/file.txt",ios::app);
+
+        if(!fileA.is_open())
+        {
+            cout<<"Система не нашла файл"<<endl;
+            return 1;
+        }
+
+        while (true)
+        {
+            getline(cin,line);
+
+            if(line == "*")
+            {
+                break;
+            }
+
+            
+            fileA<<line<<endl;
+        }
+
+        fileA.close();
+        
+    }
+
+    
+    
+
+
+
 
     return 0;
 }
