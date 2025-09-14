e53129583dd9b72e914f196608c219b857553e3e
frienddrive6-afk <frienddrive6@gmail.com>
2025-09-14 15:57:04 +0300

    Course project update. Added functions for deleting songs, creating songs, editing songs, finding songs by author, finding songs by word, outputting the entire song to the terminal, and opening songs in the default editor.

--- shag/course project/main.cpp
+++ shag/course project/main.cpp
@@ -47,7 +47,7 @@ void parseSongFile(string &db_dir_path,vector<Sounds> &song_catalog)
                     {
 
                         int start_pos = line.find(':') + 1; 
-                        int end_pos = line.find(';');
+                        int end_pos = line.rfind(';');
 
                         if (start_pos != string::npos && end_pos != string::npos) 
                         {
@@ -55,7 +55,7 @@ void parseSongFile(string &db_dir_path,vector<Sounds> &song_catalog)
                             title = line.substr(start_pos, length);
                         }
 
-                        cout<<title<<endl;
+                        // cout<<title<<endl;
                         new_song.title = title;
                     }
                     line_index++;
@@ -74,7 +74,7 @@ void parseSongFile(string &db_dir_path,vector<Sounds> &song_catalog)
                             autor = line.substr(start_pos, length);
                         }
 
-                        cout<<autor<<endl;
+                        // cout<<autor<<endl;
                         new_song.author = autor;
                     }
                     line_index++;
@@ -93,7 +93,7 @@ void parseSongFile(string &db_dir_path,vector<Sounds> &song_catalog)
                             year = stoi(line.substr(start_pos, length));
                         }
 
-                        cout<<year<<endl;
+                        // cout<<year<<endl;
                         new_song.year = year;
                     }
                     line_index++;
@@ -109,8 +109,8 @@ void parseSongFile(string &db_dir_path,vector<Sounds> &song_catalog)
 
             if(!title.empty() && !autor.empty() && year != 0)
             {
-                cout<<"Ошибок чтения файла НЕТ"<<endl;
-                cout<<endl;
+                // cout<<"Ошибок чтения файла НЕТ"<<endl;
+                // cout<<endl;
 
                 new_song.title = title;
                 new_song.author = autor;
@@ -118,10 +118,11 @@ void parseSongFile(string &db_dir_path,vector<Sounds> &song_catalog)
                 new_song.source_filename = thisPath;
 
                 song_catalog.push_back(new_song);
-            }else
-            {
-                cout<<"Ошибка чтения данные не сохраняются"<<endl;
             }
+            // else
+            // {
+            //     cout<<"Ошибка чтения данные не сохраняются"<<endl;
+            // }
             
 
             
@@ -262,6 +263,288 @@ void createSound(string &db_dir_path,vector<Sounds> &song_catalog)
     fileW.close();
 }
 
+
+void displayAllSongs(vector<Sounds> &song_catalog)
+{
+    cout<<endl;
+    cout<<"\033[33m----------Начало вывода-----------\033[0m\n"<<endl;
+    int i = 1;
+    for(Sounds song : song_catalog)
+    {
+        cout<<"Песня номер -> \033[31m"<< i <<"\033[0m"<<endl;
+        cout<<"\033[34mНазвание песни: \033[32m" <<song.title<<endl;
+        cout<<"\033[34mАвтор: \033[32m"<<song.author<<endl;
+        cout<<"\033[34mГод выпуска: \033[32m"<<song.year<<"\033[0m"<<endl;
+        cout<<endl;
+        i++;
+    }
+
+
+
+    cout<<"\033[33m---------Конец--------\033[30m"<<endl;
+    cout<<"\033[0m"<<endl;
+}
+
+void deleteSong(vector<Sounds> &song_catalog)
+{
+    cout<<"\nВведите номер песни какую ходите удалить(начало с 1): ";
+    int delated_index;
+    cin>>delated_index;
+
+    string delate_path_file = song_catalog[delated_index-1].source_filename;
+
+    song_catalog.erase(song_catalog.begin()+delated_index-1);
+    filesystem::remove(delate_path_file);
+}
+
+
+void editSong(vector<Sounds> &song_catalog)
+{
+    cout<<"\nВведите номер песни изменить(начало с 1): ";
+    int redacted_index;
+    cin>>redacted_index;
+
+    ifstream fileR(song_catalog[redacted_index-1].source_filename);
+    if(!fileR.is_open())
+    {
+        cout << "ОШИБКА при открытии файла!" << endl;
+        return;
+    }
+
+    vector<string> soung_text;
+    string line;
+
+    bool is_started = false;
+    while (getline(fileR,line))
+    {
+        if(line == "-----------------------------")
+        {
+            is_started = !is_started;
+            continue;
+        }
+        if(is_started == true)
+        {
+            soung_text.push_back(line);
+        }
+    }
+    
+    fileR.close();
+
+    cout<<"Введите параметр какой хотите изменить\n1)Название песни\n2)Имя автора\n3)Год выпуска\n4)Текст песни(Прийдется писать весь заново)\nВаш выбор: ";
+    int vubor;
+    cin>>vubor;
+
+    cin.ignore();
+    switch (vubor)
+    {
+        case 1:
+            cout<<"Введите новое название песни: ";
+            getline(cin,song_catalog[redacted_index-1].title);
+            break;
+        case 2:
+            cout<<"Введите новое имя автора: ";
+            getline(cin,song_catalog[redacted_index-1].author);
+            break;
+        case 3:
+            cout<<"Введите новый год выпуска: ";
+            cin>>song_catalog[redacted_index-1].year;
+            break;
+        case 4:
+            soung_text.clear();
+
+            
+
+            cout<<"Введите * для того что бы завершить"<<endl;
+            while (true)
+            {
+                string line;
+                getline(cin,line);
+                if(line == "*")
+                {
+                    break;
+                }else{
+                    soung_text.push_back(line);
+                }
+            }
+
+            break;
+
+    }
+
+    ofstream fileW(song_catalog[redacted_index-1].source_filename);
+
+    fileW<<"Название песни:"<<song_catalog[redacted_index-1].title<<";"<<endl;
+    fileW<<"Имя автора:"<<song_catalog[redacted_index-1].author<<";"<<endl;
+    fileW<<"Год выпуска:"<<song_catalog[redacted_index-1].year<<";"<<endl;
+
+    for(int i = 0;i < 3;i++)
+    {
+        fileW<<endl;
+    }
+    fileW<<"-----------------------------"<<endl;
+
+    for(string linee : soung_text)
+    {
+        fileW<<linee<<endl;
+    }
+    fileW<<"-----------------------------"<<endl;
+
+    fileW.close();
+
+
+}
+
+
+
+void findSongsByAuthor(vector<Sounds> &song_catalog)
+{
+    cout<<"Введтие имя автора по которому бдем искать: ";
+    string author_name;
+    getline(cin,author_name);
+
+    cout<<"Этому автору пренадлежат такие песни как: "<<endl;
+    int count = 0;
+    for(Sounds name: song_catalog)
+    {
+        if(name.author == author_name)
+        {
+            cout<<count+1<<")"<<name.title<<" "<<name.year<<" года выпуска"<<endl;
+            count++;
+        }
+    }
+
+    if(count == 0)
+    {
+        cout<<"Песен связаных с этим автором не обнаружено :("<<endl;
+    }
+
+}
+
+
+
+void findSongsByWord(vector<Sounds> &song_catalog)
+{
+    cout<<"Введите слово которое вы хотите найти в песне(Ответ название песни в котором есть это слово если оно имеется): ";
+    string word;
+    cin>>word;
+
+    for(Sounds song: song_catalog)
+    {
+        string path_to_file = song.source_filename;
+
+        ifstream fileR(path_to_file);
+        if(!fileR.is_open())
+        {
+            cout << "ОШИБКА при открытии файла!" << endl;
+            return;
+        }
+
+
+        vector<string> soung_text;
+        string line;
+
+        bool is_started = false;
+        while (getline(fileR,line))
+        {
+            if(line == "-----------------------------")
+            {
+                is_started = !is_started;
+                continue;
+            }
+            if(is_started == true)
+            {
+                soung_text.push_back(line);
+            }
+        }
+
+
+        int count_this_word = 0;
+        for(string linee : soung_text)
+        {
+            if(linee.find(word) != string::npos)
+            {
+                count_this_word++;
+            }
+        }
+
+        if(count_this_word > 0)
+        {
+            cout<<"В песне под названием " <<song.title <<" автора "<< song.author<<" было найдено это слово " <<count_this_word<<" раз"<<endl;
+        }
+
+    }
+}
+
+
+void displayFullSong(vector<Sounds> &song_catalog)
+{
+    cout<<"Выбере способ отображение песни\n1)Вывести в терминал\n2)Открыть в текстовом редакторе по умолчанию\nВаш выбор: ";
+    int vubor;
+    cin>>vubor;
+
+
+    cout<<"Выберете номер песню которую хотите вывести(Начинается с 1): ";
+    int number;
+    cin>>number;
+
+    switch (vubor)
+    {
+        case 1:
+            {
+            cout<<"Название песни: "<<song_catalog[number-1].title<<endl;
+            cout<<"Имя автора: "<<song_catalog[number-1].author<<endl;
+            cout<<"Год выпуска: "<<song_catalog[number-1].year<<endl;
+            cout<<"\n"<<endl;
+
+            cout<<"----------Текст песни----------"<<endl;
+
+            ifstream fileR(song_catalog[number-1].source_filename);
+            if(!fileR.is_open())
+            {
+                cout << "ОШИБКА при открытии файла!" << endl;
+            }
+
+
+            vector<string> soung_text;
+            string line;
+
+            bool is_started = false;
+            while (getline(fileR,line))
+            {
+                if(line == "-----------------------------")
+                {
+                    is_started = !is_started;
+                    continue;
+                }
+                if(is_started == true)
+                {
+                    soung_text.push_back(line);
+                }
+            }
+
+            for(string linee : soung_text)
+            {
+                cout<<linee<<endl;
+            }
+
+            cout<<"----------Конец песни----------"<<endl;
+
+            fileR.close();
+
+            break;
+        }
+        case 2:
+        {
+            string command  = "xdg-open \"" + song_catalog[number-1].source_filename + "\"";
+            system(command.c_str());
+            break;
+        }
+    }
+}
+
+
+
+
 int main()
 {
 
@@ -272,7 +555,19 @@ int main()
 
     loadSongs(db_dir_path,song_catalog);
 
-    createSound(db_dir_path,song_catalog);
+    displayAllSongs(song_catalog);
+
+    // deleteSong(song_catalog);
+
+    // createSound(db_dir_path,song_catalog);
+
+    // editSong(song_catalog);
+
+    // findSongsByAuthor(song_catalog);
+
+    // findSongsByWord(song_catalog);
+
+    displayFullSong(song_catalog);
 
 
     return 0;
